7c002c1f17677cbfc05354b57f86208881d9a9ba
yunchipang <yunchipang@gmail.com>
2023-04-11 16:08:58 -0700

    Update app/build.gradle; revise toast messages of onMyLocationButtonClick and onMyLocationClick

--- app/src/main/java/edu/northeastern/pawpal/NearbyActivity.java
+++ app/src/main/java/edu/northeastern/pawpal/NearbyActivity.java
@@ -15,6 +15,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
+import android.util.Log;
 import android.widget.Toast;
 
 public class NearbyActivity extends AppCompatActivity implements
@@ -22,6 +24,8 @@ public class NearbyActivity extends AppCompatActivity implements
         OnMyLocationButtonClickListener,
         OnMyLocationClickListener,
         ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private GoogleMap map;
 
     /**
      * Request code for location permission request.
@@ -36,8 +40,6 @@ public class NearbyActivity extends AppCompatActivity implements
      */
     private boolean permissionDenied = false;
 
-    private GoogleMap map;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,11 +47,12 @@ public class NearbyActivity extends AppCompatActivity implements
 
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(@NonNull GoogleMap googleMap) {
         map = googleMap;
         map.setOnMyLocationButtonClickListener(this);
         map.setOnMyLocationClickListener(this);
@@ -76,7 +79,7 @@ public class NearbyActivity extends AppCompatActivity implements
 
     @Override
     public boolean onMyLocationButtonClick() {
-        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, R.string.my_location_button_clicked_msg, Toast.LENGTH_SHORT).show();
         // Return false so that we don't consume the event and the default behavior still occurs
         // (the camera animates to the user's current position).
         return false;
@@ -84,13 +87,15 @@ public class NearbyActivity extends AppCompatActivity implements
 
     @Override
     public void onMyLocationClick(@NonNull Location location) {
-        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+        String latLongStr = location.getLatitude() + ", " + location.getLongitude();
+        Toast.makeText(this, "Current location:\n" + latLongStr, Toast.LENGTH_LONG).show();
     }
 
     // [START maps_check_location_permission_result]
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
             return;
         }
