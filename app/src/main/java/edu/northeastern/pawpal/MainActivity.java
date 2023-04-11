777fd979f4b1a3a06c1646575fff1886bdfad979
yunchipang <yunchipang@gmail.com>
2023-04-11 00:40:55 -0700

    Add bottom navigation bar

--- app/src/main/java/edu/northeastern/pawpal/MainActivity.java
+++ app/src/main/java/edu/northeastern/pawpal/MainActivity.java
@@ -1,11 +1,17 @@
 package edu.northeastern.pawpal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +20,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button nearbyButton = findViewById(R.id.nearbyButton);
-        nearbyButton.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, NearbyActivity.class);
-            startActivity(intent);
+        BottomNavigationView bottomNavigationView;
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        return true; // TODO: navigate to Home
+                    case R.id.navigation_post:
+                        return true; // TODO: navigate to Post
+                    case R.id.navigation_nearby:
+                        Intent intent = new Intent(MainActivity.this, NearbyActivity.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.navigation_inbox:
+                        return true; // TODO: navigate to Inbox
+                    case R.id.navigation_profile:
+                        return true; // TODO: navigate to Profile
+                }
+                return false;
+            }
+
         });
     }
 }
