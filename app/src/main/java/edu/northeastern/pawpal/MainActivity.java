46c9b26a61f3d3fefb067cef6de189e2d6fd08fa
yunchipang <yunchipang@gmail.com>
2023-04-10 18:45:04 -0700

    Fix .gitignore

---
+++ app/src/main/java/edu/northeastern/pawpal/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.northeastern.pawpal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/edu/northeastern/pawpal/NearbyActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.pawpal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NearbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nearby);
+    }
+}
