f723db54b3494af07c060410d48694de02b45211
dcmxlucky <dcmx1996@gmail.com>
2023-04-11 20:37:30 -0700

    Merge changes from main into new branch

---
+++ app/src/main/java/edu/northeastern/pawpal/AnimationStart.java
@@ -0,0 +1,14 @@
+package edu.northeastern.pawpal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnimationStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation_start);
+    }
+}
