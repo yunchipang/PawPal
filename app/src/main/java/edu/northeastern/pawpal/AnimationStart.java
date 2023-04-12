0aa0d99a0702f6edfb942ab4d9e0ec1f4f19dc7d
dcmxlucky <dcmx1996@gmail.com>
2023-04-11 20:38:35 -0700

    Merge changes from main into new branch

--- app/src/main/java/edu/northeastern/pawpal/AnimationStart.java
+++ app/src/main/java/edu/northeastern/pawpal/AnimationStart.java
@@ -2,13 +2,59 @@ package edu.northeastern.pawpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class AnimationStart extends AppCompatActivity {
+
+    Animation keleStart;
+    ImageView kele;
+//    TextView slogan;
+    SharedPreferences onBoardingScreen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation_start);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        final FirebaseUser user = auth.getCurrentUser();
+
+        kele = findViewById(R.id.logo);
+
+        keleStart = AnimationUtils.loadAnimation(this, R.anim.kele_ani);
+
+        //set animations
+        kele.setAnimation(keleStart);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                onBoardingScreen = getSharedPreferences("onBoardingScreen", MODE_PRIVATE);
+                boolean isFirstTime = onBoardingScreen.getBoolean("firstTime", true);
+                // TODO: insert login users
+//                if (user == null) {
+//                    startActivity(new Intent(SplashActivity.this, ReplacerActivity.class));
+//
+//                } else {
+//                    startActivity(new Intent(SplashActivity.this, ExploreActivity.class));
+//
+//                }
+                finish();
+            }
+        }, 2500);
+
+
     }
 }
--- app/src/main/java/edu/northeastern/pawpal/MainActivity.java
+++ app/src/main/java/edu/northeastern/pawpal/MainActivity.java
@@ -7,9 +7,8 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
-
+//import android.widget.Button;
+//import android.widget.TextView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
@@ -30,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.navigation_home:
                         return true; // TODO: navigate to Home
                     case R.id.navigation_post:
+                        startActivity(new Intent(MainActivity.this, AnimationStart.class));
                         return true; // TODO: navigate to Post
                     case R.id.navigation_nearby:
                         Intent intent = new Intent(MainActivity.this, NearbyActivity.class);
